4651a3d03d088a1c6e7b717ecfb8b753b7681696
santiagomary1004 <22620262@tlaxiaco.tecnm.mx>
2025-03-26 17:49:15 -0600

    Modificando recorrido

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import recorrido from './components/recorrido.vue';
 </script>
 
 <template>
-  
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+   <recorrido/>
 </template>
 
 <style scoped>
